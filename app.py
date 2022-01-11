eddcae3e3fad56b5cdd51ebe79d71f2e7b77a7dd
Mike Piech <mpiech@alum.mit.edu>
2022-01-10 21:41:00 -0800

    environmental variablized secrets

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template
 from markupsafe import escape
 import pymongo
+import os
 from waitress import serve
-#import dns
 
 app = Flask(__name__, static_url_path='')
 
-# AIzaSyARqfACf-Q-48tihr5CLHnRdrPKPQ2L984
+atlashost = os.environ['ATLAS_HOST']
+atlasusr = os.environ['ATLAS_USERNAME']
+atlaspwd = os.environ['ATLAS_PASSWORD']
+atlasdb = os.environ['ATLAS_DB']
+gmapskey = os.environ['GMAPS_KEY']
 
-client = pymongo.MongoClient("mongodb+srv://atlas-db-user-1641355160695861880:a1)9=25v@cluster0.brwd1.mongodb.net/mystrk?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://" + atlasusr + ":" + atlaspwd + "@" + atlashost + "/" + atlasdb + "?retryWrites=true&w=majority")
 db = client.mystrk
 coll = db.tracks
 trk = coll.find_one()
 
-
 @app.route("/")
 def index():
     # return app.send_static_file('index.html')
-    return render_template('index.html.jinja', name=trk, googlemapskey="AIzaSyARqfACf-Q-48tihr5CLHnRdrPKPQ2L984")
+    return render_template('index.html.jinja', name=trk, googlemapskey=GMAPS_KEY)
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -36,4 +39,4 @@ def show_subpath(subpath):
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
-    
+
