6a6e0e57ea68318ea700c80decd0c826516187f9
Mike Piech <mpiech@alum.mit.edu>
2022-01-11 20:59:37 -0800

    typecast args.get to str

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def hanndler_get_test():
 
 @app.route("/resdates", methods=["GET"])
 def handler_get_resdates():
-    start = request.args.get('start')
+    start = request.args.get('start', type=str)
     end = request.args.get('end')
     resdates = create_pandas_table(
         "SELECT DISTINCT res_date FROM reservations WHERE \
