ea027471063d01acf2f82b7795271c93ffed8ee7
Mike Piech <mpiech@alum.mit.edu>
2022-01-11 21:23:02 -0800

    split out SQL string build

--- app.py
+++ app.py
@@ -63,11 +63,9 @@ def hanndler_get_test():
 def handler_get_resdates():
     start = request.args.get('start', type=str)
     end = request.args.get('end')
-    resdates = create_pandas_table(
-        "SELECT DISTINCT res_date FROM reservations WHERE \
-         CAST (res_date AS DATE) >= CAST (" + start +
-        " AS DATE)"
-        )
+    sqlstr = "SELECT DISTINCT res_date FROM reservations WHERE \
+    CAST (res_date AS DATE) >= CAST (" + start + " AS DATE)"
+    resdates = create_pandas_table(sqlstr)
     return resdates
 
 
